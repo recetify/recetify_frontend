a1283026f41019249269cebc56521f02b513121d
BarbaraQP15 <u202211800@upc.edu.pe>
2024-11-12 14:32:23 -0500

    fix:toolbar and routes of views

--- src/router/index.js
+++ src/router/index.js
@@ -10,16 +10,14 @@ import SignInComponent from "@/iam/pages/sign-in.component.vue";
 import SignUpComponent from "@/iam/pages/sign-up.component.vue";
 import {authenticationGuard} from "@/iam/services/authentication.guard.js";
 
-
+//Rutas de las seccions de la Apliacion
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/my-account', component: accountManagementComponent, meta: { title: 'Account Management' }, },
-        { path: '/exercises', component: exercisesCardListComponent, meta: { title: 'Exercise Card List' }, },
-        { path: '/nutritions', component: nutritionsCardListComponent, meta: { title: 'Nutrition Card List' }, },
-        { path: '/products', component: productsCardListComponent, meta: { title: 'Products Card List' }, },
-        { path: '/subscription-management', component: subscriptionManagementComponent, meta: { title: 'Subscription Management' } },
-        { path: '/payment-gateway', component: paymentGatewayComponent, meta: { title: 'Payment Gateway' } },
+        { path: '/my-account', component: accountManagementComponent, meta: { title: 'Mi Cuenta' }, },
+        { path: '/exercises', component: exercisesCardListComponent, meta: { title: 'Compras' }, },
+        { path: '/nutritions', component: nutritionsCardListComponent, meta: { title: 'Recetas' }, },
+        { path: '/products', component: productsCardListComponent, meta: { title: 'Comunidad' }, },
         { path: '/sign-in', name: 'sign-in', component: SignInComponent, meta: { title: 'Sign In' } },
         { path: '/sign-up', name: 'sign-up', component: SignUpComponent, meta: { title: 'Sign Up' } },
         { path: '/',  redirect: '/my-account' },
